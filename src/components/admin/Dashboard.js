4e1f81601ca506addef6ea66aa9bc57fe18dbdc0
AKASH G K <akashgk@gmail.com>
2024-07-17 20:01:47 +0530

    Added Booking

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -40,7 +40,7 @@ const Dashboard = () => {
     <div className="flex-1 md:pl-72 md:pr-8">
       <div className="container mx-auto mt-8 md:mt-3 mb-4 px-4">
         <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           {/* Visitors Card */}
           <div className="bg-green-200 shadow-md rounded-md p-6 text-gray-800">
             <h2 className="text-xl font-semibold mb-4">Visitors</h2>
